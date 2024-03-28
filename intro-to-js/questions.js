30247d922e05098672e5b3ed82e56a74ae9e5119
George Jones <georgekeesjones@gmail.com>
2024-03-28 15:25:11 +0000

    Create questions.js
    Added Intro to JS Questions

---
+++ intro-to-js/questions.js
@@ -0,0 +1,36 @@
+// Prerequisites: You must have Installed Node
+// To run your code open a terminal and use the command 'node questions.js'
+
+// ? 1. Declare a variable that cannot be changed and log it's value
+
+
+// ? 2. Declare a variable that can be changed and log it's value
+
+
+// ? 3. Reassign the variable that you made in question 2 and log it's value
+
+
+// ? 4. Declare a variable called string with the value "javascriptissoawesome"
+
+
+// ? 5. Declare a variable called newString and using the variable in question 4 make it look like this: 'JavaScript is so awesome'
+// Hint is there a way to slice up the original string?
+
+
+// ? 6. Declare an extra string and attach it to the end of newString. e.g. JavaScript is so awesome, I can't wait to do more
+
+
+// ? 6.5. BONUS Question: Log a random character from the extra string
+
+
+// ? 7. Log the result of twenty multiplied by five
+
+
+// ? 8. Log the remainder of 10 devided by 4
+
+
+// ? 9. Declare a variable with the value 6.654321
+
+
+// ? 10. Round this to two decimal places and console log it out.
+// You may need to use Google here
